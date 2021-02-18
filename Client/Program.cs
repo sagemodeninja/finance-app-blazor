c6baa8b8185a58445750cecb1acbd8b5bb020db3
Gary Antier <contact@garyantier.com>
2021-02-18 11:59:12 +0800

    Feat: Add header and account menu.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using BlazorFluentUI;
 
 namespace FinanceApp.Client
 {
@@ -17,6 +15,8 @@ namespace FinanceApp.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddBlazorFluentUI();
 
             builder.Services.AddHttpClient("FinanceApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
@@ -30,6 +30,16 @@ namespace FinanceApp.Client
                 options.ProviderOptions.DefaultAccessTokenScopes.Add("api://01c9ae72-0e14-4cd9-8d3f-4d76ba96ba1a/api.access");
                 options.ProviderOptions.LoginMode = "redirect";
             });
+
+            #region refer: https://docs.microsoft.com/en-us/aspnet/core/blazor/security/webassembly/graph-api?view=aspnetcore-5.0#graph-sdk
+
+            builder.Services.AddScoped<GraphAPIAuthorizationMessageHandler>();
+
+            builder.Services.AddHttpClient("GraphAPI",
+                    client => client.BaseAddress = new Uri("https://graph.microsoft.com"))
+                .AddHttpMessageHandler<GraphAPIAuthorizationMessageHandler>();
+
+            #endregion
 
             await builder.Build().RunAsync();
         }
